aea65d5397ac306160372f15a48b5d0aa3a9dabe
HazmiHazim <hazimnetto01@gmail.com>
2023-09-05 08:30:11 +0800

    Fix Frontend

--- resources/views/company/admin/promotion-discount/edit.blade.php
+++ resources/views/company/admin/promotion-discount/edit.blade.php
@@ -10,6 +10,16 @@
 
             <main>
 
+                @error('error-message')
+                    <div class="error-message left-red">
+                        <i class='bx bxs-x-circle'></i>
+                        <div class="text">
+                            <span>Error</span>
+                            <span class="message">{{ $message }}</span>
+                        </div>
+                    </div>
+                @enderror
+
                 <div class="header">
                     <div class="left">
                         <h1>Edit Coupon</h1>
@@ -18,7 +28,7 @@
 
                 <div class="edit-section">
 
-                    <form action="/" method="POST">
+                    <form action="{{ route('promotion-discount.update', $promotion->id) }}" method="POST">
 
                         @method('PUT')
 
@@ -36,13 +46,22 @@
                                     <input type="text" name="name" placeholder="{{ $promotion->coupon_name }}">
 
                                     <span>Discount</span>
-                                    <input type="text" name="discount" placeholder="{{ $promotion->discount * 100}}">
+                                    <input type="text" name="discount" placeholder="{{ $promotion->discount * 100 }}">
 
                                     <span>Redeem Status</span>
                                     <input type="text">
 
                                     <span>Event</span>
-                                    <input type="text">
+                                    <div class="dropdown">
+                                        <div class="select">
+                                            <span class="selected">{{ $event->event_name }}</span>
+                                            <div class="caret"><i class='bx bx-chevron-down'></i></div>
+                                        </div>
+                                        <ul class="menu">
+                                            <li data-value="{{ $event->id }}">{{ $event->event_name }}</li>
+                                        </ul>
+                                        <input type="hidden" name="category_id" value="" required>
+                                    </div>
 
                                     <span>Validity Date</span>
                                     <input type="datetime-local" name="validity" placeholder="Enter New Validity Date">
