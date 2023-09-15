2f9146ee87a54cfcefd09564d6674d5e66b6428b
HazmiHazim <hazimnetto01@gmail.com>
2023-09-15 22:50:06 +0800

    Fixed Staff Authenticate Constructor

--- app/Http/Controllers/Staff/StaffController.php
+++ app/Http/Controllers/Staff/StaffController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class StaffController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('isStaff');
+    }
+
     public function index() : View
     {
         return view('company.staff.dashboard');
