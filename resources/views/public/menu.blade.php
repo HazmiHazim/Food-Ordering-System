43861b962f2c8f55e6c7942fdbde3a23d068a7dd
HazmiHazim <hazimnetto01@gmail.com>
2023-09-16 11:29:28 +0800

    Fix Public Frontend

--- resources/views/public/menu.blade.php
+++ resources/views/public/menu.blade.php
@@ -4,36 +4,47 @@
 
 @section('content')
 
-    <section>
+    <div class="menu-page">
 
-        <div class="menu-page">
+        <section>
 
             <main>
 
-                <div class="container">
+                <div class="page">
 
-                    <div class="title">
-                        <h1>Classic</h1>
-                        <i class='bx bx-filter'></i>
-                    </div>
+                    @if ($menu->isEmpty())
 
-                    <div class="food-item">
-
-                        @foreach ($menu as $menu)
-                            <div class="item">
-                                <img src="{{ $menu->image }}" alt="test">
-                                <span class="food-name">{{ $menu->name }}</span>
-                                <span class="price">RM {{ $menu->price }}</span>
-                                <button type="button" class="add-to-cart" data-food-id="{{ $menu->id }}" 
-                                    data-food-image="{{ $menu->image }}" data-food-name="{{ $menu->name }}" 
-                                    data-food-price="{{ $menu->price }}">
-                                    <i class='bx bx-plus'></i>
-                                    <span>Add to Cart</span>
-                                </button>
+                    <div class="container-empty">
+                        <i class='bx bxs-error-alt'></i>
+                            <div class="text">
+                                <span class="top">Sorry, no menu available at the moment.</span>
+                                <span class="bottom">Please check back later!</span>
                             </div>
-                        @endforeach
-
                     </div>
+
+                    @else
+
+                        <div class="title">
+                            <h1>Classic</h1>
+                            <i class='bx bx-filter'></i>
+                        </div>
+                        <div class="food-item">
+                            @foreach ($menu as $menu)
+                                <div class="item">
+                                    <img src="{{ $menu->image }}" alt="test">
+                                    <span class="food-name">{{ $menu->name }}</span>
+                                    <span class="price">RM {{ $menu->price }}</span>
+                                    <button type="button" class="add-to-cart" data-food-id="{{ $menu->id }}"
+                                        data-food-image="{{ $menu->image }}" data-food-name="{{ $menu->name }}"
+                                        data-food-price="{{ $menu->price }}">
+                                        <i class='bx bx-plus'></i>
+                                        <span>Add to Cart</span>
+                                    </button>
+                                </div>
+                            @endforeach
+                        </div>
+
+                    @endif
 
                 </div>
 
@@ -41,8 +52,8 @@
 
             @include('public.modal.success-message');
 
-        </div>
+        </section>
 
-    </section>
+    </div>
 
 @endsection
